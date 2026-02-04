4de9b30fab655534f1f0bb6db7330e7ae6896675
vladiiii44 <vladhockey1602006@gmail.com>
2026-02-04 20:19:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import os
 app = Flask(__name__, 
             static_folder='static',
             template_folder='templates')
-app.config.from_object(Config)
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
 
 CONTACT_INFO = {
     'name': 'Павел',
